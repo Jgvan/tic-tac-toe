baaa50daaac1f7cf3f09b25fcfddd4efe373344b
Jogvan <jogvan@gmail.com>
2022-03-16 21:42:09 +0000

    Add win/tie check to end the game.
    Two players functionality completed

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
 const gameBoard = (() => {
     let gridArray = Array(9).fill("");
 
-    const play = selectedValue => {
-        //Do stuff when player picked a square
-    }
-
+    //Clear the array
     const resetGame = () => {
         gridArray = Array(9).fill("");
     }
 
-    const checkForWinner = () => {
-        //Check if someone won the thing
+    //Did anyone win?
+    const checkForWinner = marker => {
+        const winningCombos = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for (let i = 0; i < winningCombos.length; i++) {
+            if ((gridArray[winningCombos[i][0]] === marker) && (gridArray[winningCombos[i][1]] === marker) && (gridArray[winningCombos[i][2]] === marker)) {
+                displayController.setGameText(playerController.getActivePlayer().getName() + " wins!");
+                return true;
+            }
+        }
+        return false;
     }
 
-    return { play, resetGame }
+    //Add marker to the array
+    const addMarker = (position, marker) => {
+        gridArray[parseInt(position)] = marker;
+        if (checkForWinner(marker)) {
+            gameController.gameOver();
+        }
+        else if (!gridArray.includes("")) {
+            gameController.gameOver();
+            displayController.setGameText("It's a tie!");
+        }
+    }
+
+    return { resetGame, addMarker }
 })();
 
+//Player object
 const player = (marker, playerName, isAI) => {
     const getName = () => playerName;
     const getMarker = () => marker;
@@ -28,6 +56,7 @@ const player = (marker, playerName, isAI) => {
 const displayController = (() => {
 
     const gameGrid = document.querySelector(".game-grid");
+    const gameText = document.querySelector(".game-text");
 
     const addGridEventListener = () => {
         gameGrid.addEventListener("click", placeMarker);
@@ -52,16 +81,20 @@ const displayController = (() => {
         squares.forEach((square) => {
             square.classList.add("unselected");
         });
+        gameBoard.resetGame();
     }
+
+    const setGameText = msg => gameText.textContent = msg;
 
     const placeMarker = event => {
         if (event.target.classList.contains("unselected")) {
             event.target.classList.remove("unselected");
+            gameBoard.addMarker(event.target.dataset.value, playerController.getActivePlayer().getMarker());
             playerController.switchActivePlayer();
         }
     }
 
-    return { toggleSquareSelection, resetGrid, addGridEventListener, removeGridEventListener }
+    return { toggleSquareSelection, resetGrid, addGridEventListener, removeGridEventListener, setGameText }
 })();
 
 
@@ -90,7 +123,7 @@ const playerController = (() => {
         if (activePlayer.isAI) {
             //TODO: If AI then run AI pick command
         }
-        displayController.toggleSquareSelection(activePlayer.getMarker());
+        if (gameController.getGameStatus()) { displayController.toggleSquareSelection(activePlayer.getMarker()); }
     }
 
     const getActivePlayer = () => activePlayer;
@@ -99,7 +132,8 @@ const playerController = (() => {
 })();
 
 const gameController = (() => {
-
+    let gameRunning = false;
+    
     //Starts a new player vs player game
     const newTwoPlayerGame = () => {
         const pOne = player("X", "Player One", false);
@@ -121,8 +155,20 @@ const gameController = (() => {
     const start = () => {
         displayController.resetGrid();
         displayController.toggleSquareSelection("X");
+        displayController.addGridEventListener();
+        displayController.setGameText("");
+        gameRunning = true;
     }
-    return { newTwoPlayerGame, newSinglePlayerGame }
+
+    const gameOver = () => {
+        displayController.toggleSquareSelection("");
+        displayController.removeGridEventListener();
+        gameRunning = false;
+    }
+
+    const getGameStatus = () => gameRunning;
+
+
+    return { newTwoPlayerGame, newSinglePlayerGame, gameOver, getGameStatus }
 })();
 
-displayController.addGridEventListener();
